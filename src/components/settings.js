a980d1c88a91f4ec9387b124c1cf3ce1599c0a6a
kmab <kmabadshah@gmaila.com>
2021-05-06 03:44:01 +0600

    implemented logic for add friend button

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,16 +1,47 @@
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 import {Link} from "react-router-dom";
 import {FaUserPlus} from "react-icons/fa";
 import {useForm} from "react-hook-form";
+import axios from "axios";
+import {Context} from "../app";
+import { ErrorMessage } from '@hookform/error-message';
 
 export default function Settings() {
 	const [inputDialogVisible, setInputDialogVisible] = useState(true)
 	const { register, handleSubmit, setError, clearErrors, reset, formState: { errors } } = useForm()
+	const {user} = useContext(Context)
 	
-	const onAddFriend = (d) => {
-		console.log(d)
-		reset()
-		setInputDialogVisible(false)
+	const onAddFriend = async (d) => {
+		const url = "http://localhost:8080/api/users/" + d.funame
+		
+		try {
+			const res = await axios.get(url, { validateStatus: status => status === 200 || status === 404 })
+			
+			if (res.status === 200) {
+				clearErrors()
+				
+				const url = "http://localhost:8080/api/friends"
+				
+				const res = await axios.post(url, {
+					srcID: user.id,
+					tarID: res.data.id
+				}, {validateStatus: status => status === 200})
+				
+				console.log(res.data)
+				
+				reset()
+				setInputDialogVisible(false)
+			} else {
+				setError(`funame`, {
+					type: "invalid_uname",
+					message: "Please choose a different name"
+				})
+			}
+			
+		} catch(err) {
+			console.error(err)
+		}
+		
 	}
 	
 	return (
@@ -27,11 +58,16 @@ export default function Settings() {
 				</button>
 				
 				{inputDialogVisible && (
-					<form onSubmit={handleSubmit(onAddFriend)} className="absolute w-60 h-20 bg-gray-400 m-auto left-0 right-0 top-0 bottom-0">
+					<form onSubmit={handleSubmit(onAddFriend)} className="absolute w-60 h-40 bg-gray-400 m-auto left-0 right-0 top-0 bottom-0 flex flex-col">
 						<input type="text" className={`w-full h-1/2`}
 						       {...register(`funame`, { required: true })}
 						/>
-						<div className="flex h-1/2">
+						<ErrorMessage
+							errors={errors}
+							name={`funame`}
+							render={({ message }) => <p className={`text-black text-sm mt-1`}>{message}</p>}
+						/>
+						<div className="flex flex-grow borderize">
 							<input type="submit" value="Submit"
 							       className={`w-1/2 bg-transparent border-r-2 border-white`}
 							/>
